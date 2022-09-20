2b345135091a40ce681a590fc2035c9129e198d5
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-09-20 10:27:56 -0500

    Update and rename RomanNumerals.kt to Romans.kt

---
+++ week03/Romans.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+      print("Enter a number between 1 and 10")
+      val number = Integer.valueOf(readLine())
+
+
+     if (number == 1) {
+        print("The Roman value is I.")
+    }
+    if (number == 2) {
+        print("The Roman value is II.")
+    }
+    if (number == 3) {
+        print("The Roman value is III.")
+    }
+    else print("Your number is out of range.")
+
+}
