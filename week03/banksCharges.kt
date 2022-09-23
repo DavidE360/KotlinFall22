d59141d046b83d13a048c0dd8aa2ecb684712f1b
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-09-23 15:29:46 -0500

    Update banksCharges.kt

--- week03/banksCharges.kt
+++ week03/banksCharges.kt
@@ -1,1 +1,20 @@
+fun main(){
+    
+    baseFee = 10
+    print("Enter number of cheks: ")
+    val numOfChecks = Integer.valueOf(readLine())
 
+    if (numOfChecks < 20) {
+        total = (baseFee + numOfChecks * 0.10)
+    } else if (numOfChecks >= 20 && numOfChecks <= 39) {
+        total = (baseFee + numOfChecks * 0.0)
+    } else if (numOfChecks >= 40 && numOfChecks <= 59) {
+        total = (baseFee + numOfChecks * 0.06)
+    } else {
+        total = (baseFee + numOfChecks * 0.04)
+    }
+
+
+    println("Service fees for the month: ")
+
+}
