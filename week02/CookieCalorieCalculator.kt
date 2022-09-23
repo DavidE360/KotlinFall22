0f62050a16eb9fe4ef2f9d5fef446a6d6363944d
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-09-23 16:27:13 -0500

    Update CookieCalorieCalculator.kt

--- week02/CookieCalorieCalculator.kt
+++ week02/CookieCalorieCalculator.kt
@@ -1,19 +1,9 @@
-import java.util.Scanner;
- 
-public class Cookies {
- 
-  public static void main(String[] args) {
- 
-    Scanner sc = new Scanner(System.in);
- 
-    System.out.print("Enter how many cookies you ate: ");
- 
-    int cookieCount = sc.nextInt();
- 
-    int servingSize = 40/10;
-    int caloriesPerCookie = 300/servingSize;
-    int totalCalories = cookieCount * caloriesPerCookie;
- 
-    System.out.println("Total calories consumed: " + totalCalories);
-  }
+fun main(0){
+
+    print("Enter number of cookies eaten: ")
+    val cookies = Integer.valueof(readLine())
+    val total Calories = cookies * 75
+    print("Your calorie intake was: $totalCalories")
+
+
 }
