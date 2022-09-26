a2a4eed1fa91aeb04a68dac59f72eb5f6b2aa32c
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-09-26 13:18:34 -0500

    Update restuarantSelector.kt

--- week03/restuarantSelector.kt
+++ week03/restuarantSelector.kt
@@ -1,2 +1,42 @@
 fun main(){
+  
+  println("Is anyone in your party a vegetarian?")
+  val veggieChoice = readline()!!
+  println("Is anyone in your party a vegan?")
+  val veganChoice = readline()!!
+  println("Is anyone in your party gluten-free?")
+  val glutenChoice = readline()!!
+  
+  println("Here are your restaurant choices: ")
+  
+  if (veggieChoice.equals("Yes",ignoreCase = true)){
+    if (veganChoice.equals("Yes", ignoreCase = true)){
+      if(glutenChoice.equals("Yes", ignoreCase = true)){
+        printCafe()
+        printKitchen()
+      }
+    }
+    else{
+      if(glutenChoice.equals("Yes", ignoreCase = true)){
+        printCompany()
+      }
+      else{
+        printItalian()
+      }
+    }
+  }
+  
+}
+
+fun printJoe(){
+  println("\Joe's Gourmet Burgers ")
+}
+fun printCompany(){
+  println("\tMain Street Pizza Company ")
+}
+fun printItalian(){
+  println("\tMama's Fine Italian ")
+}
+fun printKitchen(){
+  println("\tThe Chef's Kitchen ")
 }
