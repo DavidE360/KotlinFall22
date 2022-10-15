bbaf621f22581f052a555a0b4dae5c35d624f316
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-15 15:23:40 -0500

    Update massWeight.kt

--- week03/massWeight.kt
+++ week03/massWeight.kt
@@ -1,13 +1,15 @@
 fun main(){
-    print("Enter mass in Newtons: ")
+    println("Enter the mass of an object")
     var mass = Integer.valueOf(readLine())
-    if (mass>1000){
-        println("Mass is too heavy")
-    }else{
-        if(mass>10){
-            println("Mass is too light.")
-        }
+    var weight = mass * 9.8
+
+    println("The weight is $weight")
+
+    if (weight > 1000){
+        println("Object is too heavy")
     }
-    var weight = (mass 8*9.8)
-    println("The weight is: $weight")
+
+    if(weight <10){
+        println("Object is too light")
+    }
 }
