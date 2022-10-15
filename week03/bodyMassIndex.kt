fc825ba6714809989b502123401524194ceb29da
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-15 15:21:05 -0500

    Update bodyMassIndex.kt

--- week03/bodyMassIndex.kt
+++ week03/bodyMassIndex.kt
@@ -1,10 +1,21 @@
-import  kotlin.math.pow
+import kotlin.math.pow
 
 fun main(){
     print("Enter your weight (in pounds): ")
     val weight = Integer.valueOf(readLine())
     print("Enter your height (in inches): ")
     val height = Integer.valueOf(readLine())
-    val bmi = (weight * 703) / height.toDouble().pow( n: 2)
-    print(bmi)
+    val bmi = (weight*703)/height.toDouble().pow(2)
+
+    var weightClass = ""
+    if (bmi >= 18.5 && bmi <= 25){
+        weightClass = "optimal weight"
+    }
+    if(bmi < 18.5){
+        weightClass = "underweight"
+    }
+    if(bmi > 25){
+        weightClass = "overweight"
+    }
+    print("Your bmi is $bmi and you are $weightClass")
 }
