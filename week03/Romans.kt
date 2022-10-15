1237784dca726677c69b50f881c6a58188d637c3
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-15 14:38:10 -0500

    Update Romans.kt

--- week03/Romans.kt
+++ week03/Romans.kt
@@ -1,18 +1,18 @@
 fun main(){
-
-      print("Enter a number between 1 and 10")
-      val number = Integer.valueOf(readLine())
-
-
-     if (number == 1) {
-        print("The Roman value is I.")
+    println("Enter a number between 1 & 10 >> ")
+    var deviceInput = readLine()
+    val deviceInt = deviceInput?.toInt()
+    var romanNumeral: String = when(deviceInt){
+        1 -> "I"
+        2 -> "II"
+        3 -> "III"
+        4 -> "IV"
+        5 -> "V"
+        6 -> "VI"
+        7 -> "VII"
+        8 -> "VIII"
+        9 -> "IX"
+        10 -> "X"
+        else -> "Error. The number is out of bounds"
     }
-    if (number == 2) {
-        print("The Roman value is II.")
-    }
-    if (number == 3) {
-        print("The Roman value is III.")
-    }
-    else print("Your number is out of range.")
-
 }
