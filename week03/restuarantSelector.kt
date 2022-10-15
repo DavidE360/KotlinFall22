0e90deb070b07a9682fbc4d733af0ccba1b7cbe6
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-15 15:47:14 -0500

    Update restuarantSelector.kt

--- week03/restuarantSelector.kt
+++ week03/restuarantSelector.kt
@@ -1,42 +1,59 @@
 fun main(){
-  
-  println("Is anyone in your party a vegetarian?")
-  val veggieChoice = readline()!!
-  println("Is anyone in your party a vegan?")
-  val veganChoice = readline()!!
-  println("Is anyone in your party gluten-free?")
-  val glutenChoice = readline()!!
-  
-  println("Here are your restaurant choices: ")
-  
-  if (veggieChoice.equals("Yes",ignoreCase = true)){
-    if (veganChoice.equals("Yes", ignoreCase = true)){
-      if(glutenChoice.equals("Yes", ignoreCase = true)){
-        printCafe()
-        printKitchen()
-      }
+// Aaryan Khan assignment number 03.7 Restaurant Selector 09/22/2022
+    println("Is anyone in your party a vegetarian? ")
+    val vegetChoice = readLine()!!
+    println("Is anyone in your party a vegan? ")
+    val veganChoice = readLine()!!
+    println("Is anyone in your party gluten-free? ")
+    val glutenChoice = readLine()!!
+
+    println("Here are your restaurant choices:")
+
+    if (vegetChoice.equals("yes",ignoreCase = true)){
+        if (veganChoice.equals("yes",ignoreCase = true)){
+            if(glutenChoice.equals("yes",ignoreCase = true)){
+                printCafe()
+                printKitchen()
+            }
+        }
+        else{
+            if(glutenChoice.equals("yes",ignoreCase = true)){
+                printStreet()
+            }
+            else{
+                printItalian()
+            }
+        }
     }
+
     else{
-      if(glutenChoice.equals("Yes", ignoreCase = true)){
-        printCompany()
-      }
-      else{
-        printItalian()
-      }
+        if (veganChoice.equals("yes",ignoreCase = true)){
+            if(glutenChoice.equals("yes",ignoreCase = true)){
+            }
+        }
+        else{
+            if(glutenChoice.equals("yes",ignoreCase = true)){
+            }
+            else{
+                printJoe()
+            }
+        }
     }
-  }
-  
+
 }
 
 fun printJoe(){
-  println("\Joe's Gourmet Burgers ")
+    println("\tJoe’s Gourmet Burgers ")
 }
-fun printCompany(){
-  println("\tMain Street Pizza Company ")
+fun printStreet(){
+    println("\tMain Street Pizza Company ")
+}
+fun printCafe(){
+    println("\tCorner Café  ")
 }
 fun printItalian(){
-  println("\tMama's Fine Italian ")
+    println("\tMama’s Fine Italian  ")
 }
 fun printKitchen(){
-  println("\tThe Chef's Kitchen ")
+    println("\tThe Chef’s Kitchen ")
 }
