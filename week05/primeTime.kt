03a572660de97a1126b521379dc517d31c8cde3d
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-22 17:01:22 -0500

    Update primeTime.kt

--- week05/primeTime.kt
+++ week05/primeTime.kt
@@ -1,1 +1,18 @@
-
+fun main(){
+    println(isPrime(6))
+    println(isPrime(13))
+    println(isPrime(8893))
+}
+fun isNumberDivisible(number: Int, divisor: Int): Boolean{
+    return number % divisor == 0
+}
+fun isPrime(number: Int): Boolean {
+    if (number <= 0)
+        return false
+    for (i in 2 until number) {
+            if (isNumberDivisible(number, i)){
+                return false
+            }
+        }
+    return true
+}
