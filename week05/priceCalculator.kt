968116c381d5f414117bf9d41167d0a5dfff48c9
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-22 17:07:23 -0500

    Update priceCalculator.kt

--- week05/priceCalculator.kt
+++ week05/priceCalculator.kt
@@ -1,1 +1,10 @@
-
+fun main() {
+  println("What is the item's wholesale cost?")
+  val wholesaleCost = readLine()!!.toDouble()
+  println("What is the item's markup percentage?")
+  val markupPercentage = readLine()!!.toDouble()
+  println("Your final cost is $" calculateRetail(wholesaleCost, markupPercentage))
+}
+fun caluclateRetail(wholesaleCost: Double, markupPercentage: Double): Double{
+  return (wholesaleCast*((markupPercentage/100)+1))
+}
