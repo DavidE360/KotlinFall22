0b94684555f30f275058c5c1a2d22ec41518ddae
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-23 19:33:28 -0500

    Update fallingDistance.kt

--- week05/fallingDistance.kt
+++ week05/fallingDistance.kt
@@ -1,1 +1,10 @@
+import kotlin.math.pow
 
+fun main(){
+    for(i in 1..10){
+        println("At $i seconds the object has fallen " + fallingDistance(i) + " meters.")
+    }
+}
+fun fallingDistance(time : Int) : Double{
+    return .5*(9.8*(time.toDouble().pow(2.0)))
+}
