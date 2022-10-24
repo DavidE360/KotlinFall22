48341f668542e7bea04d4bf5a27db101fa2bbe06
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-23 19:34:42 -0500

    Update celsiusTable.kt

--- week05/celsiusTable.kt
+++ week05/celsiusTable.kt
@@ -1,1 +1,8 @@
-
+fun main(){
+    for (i in 0..20){
+        println("$i degrees in Fahrenheit is " + celsius(i.toDouble()) + " degrees in celsius.")
+    }
+}
+fun celsius(fahrenheit : Double) : Double{
+    return (fahrenheit-32)*(5.0/9)
+}
