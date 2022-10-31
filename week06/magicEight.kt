38cbb70d17682945f28ae1222e58c34a091dfa6e
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-10-30 22:06:51 -0500

    Update magicEight.kt

--- week06/magicEight.kt
+++ week06/magicEight.kt
@@ -1,1 +1,18 @@
 
+fun main(args:Array<Strings>){
+    
+    var arraylist= Arraylist<String>()
+    arraylist.add("Yes, of course!")
+    arraylist.add("Without a doubt, yes.")
+    arraylist.add("You can count on it.")
+    arraylist.add("For sure!")
+    arraylist.add("Ask me later.")
+    arraylist.add("Iâ€™m not sure.")
+    arraylist.add("I canâ€™t tell you right now. Iâ€™ll tell you after my nap. No way!")
+    arraylist.add("I donâ€™t think so.")
+    arraylist.add("Without a doubt, no.")
+    arraylist.add("The answer is clearly NO.")
+    
+    println("Responses:" + arraylist.get(0))
+    
+}
