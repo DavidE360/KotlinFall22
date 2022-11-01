905a6a1bde2cecb46a54f1f724ecc8ccd0c95864
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-11-01 08:42:44 -0500

    Create variables.kt

---
+++ week02/variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var myAge : Int  = 32
+    var averageAge : Double = 32.0
+    averageAge = (averageAge+32)/2
+    val testNumber = 1
+    val evenOdd = testNumber%2
+    var answer = 3
+    answer += 1
+    answer += 10
+    answer *= 10
+    answer -= 3
+}
