f0f844e441ff2ac4a75de23f81b74242a511b23e
DavidE360 <89318885+DavidE360@users.noreply.github.com>
2022-11-03 10:21:21 -0500

    Update textAdventure.kt

--- week06/textAdventure.kt
+++ week06/textAdventure.kt
@@ -1,100 +1,52 @@
-fun main(args: Array<String>) {
-    val game=Branch("You are in a dusty castle room.Passages lead to the north and south."){
-        WalkNorth() leadsTo TerminalBranch("You are in a dusty castle room.Passages lead to the north and south.")
-        WalkSouth() leadsTo TerminalBranch("You are in the armory.There is a room off to the south.")
-        WalkEast() leadsTo Branch("""IYou are in a torch-lit hallway.There are rooms to the east and west.""")
-        WalkWest() leadsTo TerminalBranxh("You are in a bedroom. A window overlooks the castle courtyard. A hallway is to the west."){
-            OpenAction() leadsTo TerminalBranch("A snake pop out of the box and bite you. You die of a painful poisoning!")
-            val startOfBridgeref:RefBranch=RefBranch(Branch())
-            val startOfBridge=Branch("You are facing a very old bridge."){
-                WalkNorth() leadsTo Branch("you are in the middle of a very old bridge"){
-                    WalkSouth() leadsTo startOfBridgeref
-                    WalkNorth() leadsTo Branch("You are in a dusty castle room. Passages lead to the north and south.")
-                    WalkEast() leadsTo Branch("You are in a torch-lit hallway. There are rooms to the east and west.")
-                }
+// David Emokpae
+
+fun main() {
+
+    print("What is your name? ")
+    val name: string? = readLine()
+
+    // Introduction
+    println(" Welcome, " + name + ", let us find adventure!")
+    println("You find yourself on the edge of a dark forest.")
+    println("Can you find your way through?")
+
+    do {
+        print("Would you like to step into the forest? ")
+        val playnow: string? = readline()
+        if (playnow == "no") {
+            println("You are not ready for this game...") {
+                break
             }
-            startOfBridgeref.ref=startOfBridge // Little hack to pass a reference to a closure
+            println("To your left, you see a bear.")
+            println("To your right, there is more forest.")
+            println("There is a rock wall directly in front of you.")
+            println("Behind you is the forest exit.")
 
-            WalkNorth() leadsTo startOfBridge
+            val direction: String? = readline()
+            if (direction == "no") {
+                println("You are not ready for this game...") {
+                    break
+                }
+
+                println("To your left, you see a bear.")
+                println("To your right, there is more forest.")
+                println("There is a rock wall directly in front of you.")
+                println("Behind you is the forest exit.")
+                println("What direction do you move?")
+                val response: string? = readLine()
+
+                if (response == "left") {
+                    println("The bear eats you, Farewell, " + name) {
+                        break
+                    }
+                    else if (response == "right") {
+                        println("You head deeper into the forest.")
+                    }
+
+                } while (response != "quit")
+
+
+            }
         }
-    }
-    game.process()
-}
-
-interface Action{
-    val label:String
-    fun accept(input :String):Boolean
-}
-
-abstract class SimpleAction(override val label: String):Action{
-    override fun accept(input: String) = label == input
-}
-
-class OpenAction:SimpleAction("OPEN")
-class CloseAction:SimpleAction("CLOSE")
-class HitAction:SimpleAction("HIT")
-
-abstract class WalkAction(val direction:String):Action{
-    override val label: String = "WALK $direction"
-    override fun accept(input: String): Boolean {
-        val (iaction,idirection)=input.replace(Regex.fromLiteral(" {2,}")," ").split(" ")
-        return iaction=="WALK" && direction==idirection
-    }
-}
-
-class WalkNorth:WalkAction("NORTH")
-class WalkSouth:WalkAction("SOUTH")
-class WalkEast:WalkAction("EAST")
-class WalkWest:WalkAction("WEST")
-
-interface IBranch {
-    val description: String
-    val fn: IBranch.() -> Unit
-    fun process()
-
-    infix fun Action.leadsTo(brc: IBranch)
-}
-
-
-open class Branch(override val description:String="", override val fn:IBranch.()->Unit={}) : IBranch {
-    val actions:MutableList<Pair<Action,IBranch>> = mutableListOf()
-    var visited=false
-    override fun process()  {
-        if (!visited) this.fn()
-        visited=true
-
-        println(description)
-        print("The following are available:  \n- ${actions.map { it.first.label }.joinToString("\n- ")}\n> ")
-        var input= readLine() ?: ""
-        var processed  = actions.find { it.first.accept(input) }?.second?.run { process(); true }?:false
-        while (!processed){
-            input= readLine() ?: ""
-            processed = actions.find { it.first.accept(input) }?.second?.run { process(); true }?:false
-        }
-    }
-    override infix fun Action.leadsTo(brc:IBranch){
-        actions.add(this to brc)
-    }
-}
-
-class RefBranch(var ref:Branch=Branch()):IBranch {
-    override val fn: IBranch.() -> Unit
-        get() = ref.fn
-    override val description: String
-        get() = ref.description
-
-    override fun process() {
-        ref.process()
-    }
-
-    override fun Action.leadsTo(brc: IBranch) {
-        ref.actions.add(this to brc)
-    }
-
-}
-
-class TerminalBranch(val ending:String):Branch(){
-    override fun process(){
-        println(ending)
     }
 }
